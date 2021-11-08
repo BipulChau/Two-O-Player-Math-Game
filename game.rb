71780480eb74cc9c282b1d5f7f2770fc41dd028d
Bipul Chaudhary <bipulchau@gmail.com>
2021-11-08 13:53:01 -0500

    Required all files into main.rb and it is the main entrypoint into the game

--- game.rb
+++ game.rb
@@ -1,5 +1,33 @@
-class Game
-  puts "What does 5 plus 3 equal?"
-  gets.chomp
-end
+require './player'
 
+class Game
+
+  attr_reader :report_life, :players, :current_player
+
+  def initialize (p1, p2)
+    @players = [p1, p2]
+    @current_index = 0
+    @current_player = @players[@current_index]
+  end
+  
+  def subtract_life
+    @current_player.life -= 1
+    if @current_player.life == 0
+      puts "#{@current_player.name}, you lose!" 
+      puts "--- GAME OVER ---"
+    end
+  end
+
+  def report_life
+    puts "Life Remaining ==> P1: #{@players[0].life}/3  P2:#{@players[1].life}/3"
+  end
+  
+  def change_player
+    if @current_player == @players[0]
+      @current_player = @players[1]
+    else
+      @current_player = @players[0]
+    end
+  end
+  
+end
--- main.rb
+++ main.rb
@@ -1,3 +1,38 @@
-require "./game"
+# This is the main entrypoint into the program
+# It requires the other files that it needs
 
-new_game = Game.new
+require './game'
+require './player'
+require './question'
+
+# set up players
+print "Enter Player 1 name: "
+name1 = gets.chomp.to_s
+print "Enter Player 2 name: "
+name2 = gets.chomp.to_s
+player1 = Player.new(name1)  #Using Player class from player.rb
+player2 = Player.new(name2)  #Using Player class from player.rb
+
+# set current player
+game = Game.new(player1, player2) #Using Game class from game.rb
+
+while game.players[0].life > 0 and game.players[1].life > 0 do
+  # generate question and answer
+  puts "--- NEW TURN ---"
+  question = Question.new
+  print "#{game.current_player.name}: #{question.gen_question}"
+  answer = question.answer
+
+  # prompt for answer
+  input = gets.chomp.to_i
+
+  # check answer
+  if (input != answer)
+    game.subtract_life
+  end
+
+  game.report_life
+
+  # change turn
+  game.change_player
+end
